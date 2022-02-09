61e1245bca2ef2cd24c7b911c8ee48beb5acda6c
Gustavo Jackson <gustajpe@gmail.com>
2022-02-09 08:19:43 -0300

    altera titulo

--- src/br/com/alura/git/application/MainProgram.java
+++ src/br/com/alura/git/application/MainProgram.java
@@ -5,6 +5,8 @@ public class MainProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("titulo exemplo git");
+		
 		System.out.println("Hello Git");
 		System.out.println("Hello Git versao 2");
 		System.out.println("Hello Git alteracao Ana.");
