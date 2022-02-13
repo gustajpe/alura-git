ce47cb904b6673c2c011947529e3700f78c714ea
Gustavo Jackson <gustajpe@gmail.com>
2022-02-13 09:14:55 -0300

    correcao

--- src/br/com/alura/git/application/MainProgram.java
+++ src/br/com/alura/git/application/MainProgram.java
@@ -3,12 +3,9 @@ package br.com.alura.git.application;
 public class MainProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub	
 		System.out.println("Hello Git");
 		System.out.println("Hello Git versao 2");
 		System.out.println("Hello Git alteracao Ana.");
-
 	}
-
 }
