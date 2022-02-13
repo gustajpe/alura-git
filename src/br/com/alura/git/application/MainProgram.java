d540b68fe7d58005e6d74a8dd1381ea177952bf7
Gustavo Jackson <gustajpe@gmail.com>
2022-02-13 09:20:54 -0300

    asda

--- src/br/com/alura/git/application/MainProgram.java
+++ src/br/com/alura/git/application/MainProgram.java
@@ -3,15 +3,8 @@ package br.com.alura.git.application;
 public class MainProgram {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		// TODO Auto-generated method stub	
-=======
-		// TODO Auto-generated method stub
-		
 		System.out.println("titulo exemplo git");
 		System.out.println("segundo titulo exemplo git");
-		
->>>>>>> titulo
 		System.out.println("Hello Git");
 		System.out.println("Hello Git versao 2");
 		System.out.println("Hello Git alteracao Ana.");
